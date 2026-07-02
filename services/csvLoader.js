263bba6abee2099ea8bc0ed6ac45aabc8d83c4a5
cbpgresult-com <sunflixmovies@gmail.com>
2026-07-02 16:12:21 +0530

    Create csvLoader.js

---
+++ services/csvLoader.js
@@ -0,0 +1,116 @@
+const fs = require("fs");
+const path = require("path");
+const csv = require("csv-parser");
+
+let students = [];
+
+/*
+=========================================
+LOAD CSV
+=========================================
+*/
+
+function loadCSV(callback) {
+
+    students = [];
+
+    const csvPath = path.join(
+        __dirname,
+        "..",
+        "data",
+        "students.csv"
+    );
+
+    fs.createReadStream(csvPath)
+
+        .pipe(csv())
+
+        .on("data", (row) => {
+
+            students.push(row);
+
+        })
+
+        .on("end", () => {
+
+            console.log("====================================");
+            console.log("CSV Loaded Successfully");
+            console.log("Total Records :", students.length);
+            console.log("====================================");
+
+            if (callback) callback();
+
+        })
+
+        .on("error", (err) => {
+
+            console.log("CSV ERROR");
+            console.log(err);
+
+        });
+
+}
+
+/*
+=========================================
+GET ALL STUDENTS
+=========================================
+*/
+
+function getStudents() {
+
+    return students;
+
+}
+
+/*
+=========================================
+GET STUDENT BY INDEX
+=========================================
+*/
+
+function getStudent(index) {
+
+    return students[index];
+
+}
+
+/*
+=========================================
+TOTAL RECORDS
+=========================================
+*/
+
+function totalStudents() {
+
+    return students.length;
+
+}
+
+/*
+=========================================
+RELOAD CSV
+=========================================
+*/
+
+function reloadCSV(callback) {
+
+    console.log("Reloading CSV...");
+
+    loadCSV(callback);
+
+}
+
+module.exports = {
+
+    loadCSV,
+
+    getStudents,
+
+    getStudent,
+
+    totalStudents,
+
+    reloadCSV
+
+};
